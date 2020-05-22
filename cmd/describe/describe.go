9f0d813a8e0f307b3e206b77bcb09ff4a63c15ae
Fabien Blin <fabien.blin0gmail.com>
2020-05-22 15:45:25 +0200

    first version of histogram

--- cmd/describe/describe.go
+++ cmd/describe/describe.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	desc "pkg"
 	"encoding/csv"
+	"fmt"
 	"log"
+	"math"
 	"os"
+	"sort"
+	"strconv"
+
+	"gonum.org/v1/gonum/mat"
 )
+
+var featureNames []string
+var statNames []string
 
 func main() {
 	// get csv data
@@ -33,7 +41,196 @@ func main() {
 		os.Exit(1)
 	}
 
-	description := desc.Describe(data)
+	description := describe(data)
+	printDescription(description)
+}
 
-	desc.PrintDescription(description)
+// parse data to printable statistics
+func describe(data [][]string) [][]float64 {
+	matrix := fillMatrix(data) // parse and cast data from string to float
+
+	initPrintable(data) // init feature names and stat names
+
+	// allocate stat array
+	m := make([][]float64, len(statNames))
+	for i := range m {
+		m[i] = make([]float64, len(featureNames))
+	}
+
+	// count
+	for i, _ := range featureNames {
+		m[0][i] = count(mat.Col(nil, i, matrix))
+	}
+
+	// mean
+	for i, _ := range featureNames {
+		m[1][i] = sum(mat.Col(nil, i, matrix)) / m[0][i]
+	}
+
+	// std
+	for i, _ := range featureNames {
+		m[2][i] = std(mat.Col(nil, i, matrix), m[1][i], m[0][i])
+	}
+
+	// min
+	for i, _ := range featureNames {
+		m[3][i] = min(mat.Col(nil, i, matrix))
+	}
+
+	// 25%
+	for i, _ := range featureNames {
+		m[4][i] = quartile(mat.Col(nil, i, matrix), m[0][i], 25)
+	}
+
+	// 50%
+	for i, _ := range featureNames {
+		m[5][i] = quartile(mat.Col(nil, i, matrix), m[0][i], 50)
+	}
+
+	// 75%
+	for i, _ := range featureNames {
+		m[6][i] = quartile(mat.Col(nil, i, matrix), m[0][i], 75)
+	}
+
+	// max
+	for i, _ := range featureNames {
+		m[7][i] = max(mat.Col(nil, i, matrix))
+	}
+	return m
+}
+
+func fillMatrix(data [][]string) *mat.Dense {
+	nrows := len(data)
+	ncols := len(data[0])
+	matrix := mat.NewDense(nrows-1, ncols-6, nil)
+	for i := 1; i < nrows; i++ {
+		for j := 6; j < ncols; j++ {
+			float, err := strconv.ParseFloat(data[i][j], 64)
+			if err == nil {
+				matrix.Set(i-1, j-6, float)
+			} else {
+				matrix.Set(i-1, j-6, math.NaN()) // empty fields are set to NaN
+			}
+		}
+	}
+	return matrix
+}
+
+func printDescription(description [][]float64) {
+	spacing := 15
+	// print feature names
+	fmt.Printf("%10s", " ")
+	for _, feature := range featureNames {
+		fmt.Printf("% -*s", spacing, feature)
+	}
+	fmt.Print("\n\n")
+
+	// print values
+	for i, stat := range statNames {
+		fmt.Printf("% -10s", stat)
+		for j, _ := range featureNames {
+			fmt.Printf("%-*.4f", spacing, description[i][j])
+		}
+		fmt.Print("\n")
+	}
+}
+
+func initPrintable(data [][]string) {
+	featureNames = data[0][6:]
+	for i, name := range featureNames { // shorten feature names
+		if len(featureNames[i]) > 10 {
+			featureNames[i] = name[:9] + "."
+		}
+	}
+	statNames = []string{"Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max"}
+}
+
+func count(values []float64) float64 {
+	var count float64
+
+	for _, num := range values {
+		if !math.IsNaN(num) {
+			count++
+		}
+	}
+	return count
+}
+
+func sum(values []float64) float64 {
+	var sum float64
+
+	for _, num := range values {
+		if !math.IsNaN(num) {
+			sum += num
+		}
+	}
+	return sum
+}
+
+func std(values []float64, mean float64, count float64) float64 {
+	var std float64
+	var variance float64
+
+	for _, num := range values {
+		if !math.IsNaN(num) {
+			variance += math.Pow(num-mean, 2)
+		}
+	}
+	variance /= count - 1
+	std = math.Sqrt(variance)
+	return std
+}
+
+func min(values []float64) float64 {
+	var min float64 = math.Inf(1)
+
+	for _, num := range values {
+		if !math.IsNaN(num) && min > num {
+			min = num
+		}
+	}
+	return min
+}
+
+type ByValue []float64
+
+func (a ByValue) Len() int           { return len(a) }
+func (a ByValue) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByValue) Less(i, j int) bool { return math.IsNaN(a[i]) || a[i] < a[j] }
+
+func quartile(values []float64, count float64, centil float64) float64 {
+	var quartile float64 = math.Inf(-1)
+
+	sort.Sort(ByValue(values))
+
+	var nbNan int
+	for _, num := range values {
+		if math.IsNaN(num) {
+			nbNan++
+		} else {
+			break
+		}
+	}
+
+	limit := nbNan + int(math.Round(((centil * count) / 100)))
+	for i, num := range values {
+		if i == limit {
+			break
+		}
+		if !math.IsNaN(num) && quartile < num {
+			quartile = num
+		}
+	}
+	return quartile
+}
+
+func max(values []float64) float64 {
+	var max float64 = math.Inf(-1)
+
+	for _, num := range values {
+		if !math.IsNaN(num) && max < num {
+			max = num
+		}
+	}
+	return max
 }
--- cmd/visual/histogram.go
+++ cmd/visual/histogram.go
@@ -3,20 +3,30 @@ package main
 import (
 	"encoding/csv"
 	"log"
+	"math"
 	"os"
+	"sort"
+	"strconv"
 
+	"gonum.org/v1/gonum/mat"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
 )
 
+type Value []float64
+
+func (a Value) Len() int           { return len(a) }
+func (a Value) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a Value) Less(i, j int) bool { return math.IsNaN(a[i]) || a[i] < a[j] }
+
 func main() {
 	// get csv data
 	var fileName string
 
 	if len(os.Args) != 2 {
-		log.Fatal("Usage :\n\tdescribe.exe <file.csv>")
+		log.Fatal("Usage :\n\thistogram.exe <file.csv>")
 		os.Exit(1)
 	} else {
 		fileName = os.Args[1]
@@ -37,54 +47,171 @@ func main() {
 		os.Exit(1)
 	}
 
-	description := describe(data)
+	featureNames := data[0][6:]
+	matrix := fillMatrix(data)
+	nbPortions := 10
 
-	printDescription(description)
+	houseFrequenceTab := houseDistribution(featureNames, matrix, nbPortions)
 
-	groupA := plotter.Values{20, 35, 30, 35, 27}
-	groupB := plotter.Values{25, 32, 34, 20, 25}
-	groupC := plotter.Values{12, 28, 15, 21, 8}
+	// for h, ft := range houseFrequenceTab {
+	// 	fmt.Println(h)
+	// 	for s, f := range ft {
+	// 		fmt.Println(s, f)
+	// 	}
+	// }
+
+	for _, subject := range featureNames {
+		plotHistogram(subject, houseFrequenceTab, nbPortions)
+	}
+}
+
+func houseDistribution(featureNames []string, matrix map[string]*mat.Dense, nbPortions int) map[string]map[string][]float64 {
+	houseFrequenceTab := make(map[string]map[string][]float64) // contains grade frequencies of each house in a float array (houseFrequenceTab[house][subject]=[]float64{...})
+
+	for house, houseGrades := range matrix { // loop over houses
+		_, ncols := houseGrades.Dims()
+		frequenceTab := make(map[string][]float64)
+		for i := 0; i < ncols; i++ { // loop over subject grades
+			subject := featureNames[i]
+			subjectGrades := mat.Col(nil, i, houseGrades)
+			subjectGrades = cleanTab(subjectGrades)
+
+			distrib := distribution(subjectGrades, nbPortions)
+
+			frequenceTab[subject] = distrib
+		}
+		houseFrequenceTab[house] = frequenceTab
+	}
+	return houseFrequenceTab
+}
+
+func distribution(tab []float64, nbPortions int) []float64 {
+	frequencesTab := make([]float64, nbPortions)
+	max := max(tab)
+	min := min(tab)
+	diff := max - min
+	portion := diff / float64(nbPortions)
+
+	for p := 0; p < nbPortions; p++ {
+		high := min + portion*(float64(p)+1)
+		low := min + portion*float64(p)
+		var freq float64
+		for _, val := range tab {
+			if val >= low && val <= high {
+				freq++
+			}
+		}
+		frequencesTab[p] = freq
+	}
+	return frequencesTab
+}
+
+// sort tab and eliminate NaN values
+func cleanTab(tab []float64) []float64 {
+	sort.Sort(Value(tab))
+	var nbNan int
+	for _, num := range tab {
+		if math.IsNaN(num) {
+			nbNan++
+		} else {
+			break
+		}
+	}
+	cleantab := tab[nbNan:]
+	return cleantab
+}
+
+func fillMatrix(data [][]string) map[string]*mat.Dense {
+	firstCol := 6
+	nrows := len(data)
+	ncols := len(data[0])
+	matrix := make(map[string]*mat.Dense)
+
+	for i := 1; i < nrows; i++ {
+		house := data[i][1]
+
+		if len(house) > 0 {
+			toAdd := mat.NewDense(1, ncols-firstCol, nil)
+			for j := firstCol; j < ncols; j++ { // fill the array
+				grade, err := strconv.ParseFloat(data[i][j], 64)
+
+				if err == nil {
+					toAdd.Set(0, j-firstCol, grade)
+				} else {
+					toAdd.Set(0, j-firstCol, math.NaN())
+				}
+			}
+			if matrix[house] == nil { // array init for the 4 houses
+				matrix[house] = mat.DenseCopyOf(toAdd)
+			} else {
+				var x mat.Dense
+				x.Stack(matrix[house], toAdd)
+				matrix[house] = &x
+			}
+		}
+	}
+	return matrix
+}
+
+func max(values []float64) float64 {
+	var max float64 = math.Inf(-1)
+
+	for _, num := range values {
+		if !math.IsNaN(num) && max < num {
+			max = num
+		}
+	}
+	return max
+}
+
+func min(values []float64) float64 {
+	var min float64 = math.Inf(1)
+
+	for _, num := range values {
+		if !math.IsNaN(num) && min > num {
+			min = num
+		}
+	}
+	return min
+}
+
+// create image of subject frequencies with all houses houseFrequenceTab[house][subject]=[]float64{...}
+func plotHistogram(subject string, houseFrequenceTab map[string]map[string][]float64, nbPortions int) {
+	subjectTab := make(map[string][]float64) // subjectTab[house]=[]float64{frequencies}
+	for house, subjectFreq := range houseFrequenceTab {
+		subjectTab[house] = subjectFreq[subject]
+	}
 
 	p, err := plot.New()
 	if err != nil {
 		panic(err)
 	}
-	p.Title.Text = "Bar chart"
-	p.Y.Label.Text = "Heights"
+	p.Title.Text = subject
+	p.X.Label.Text = "Note"
+	p.Y.Label.Text = "Fréquence"
 
-	w := vg.Points(20)
-
-	barsA, err := plotter.NewBarChart(groupA, w)
-	if err != nil {
-		panic(err)
+	w := vg.Points(10)
+	i := 0
+	for house, frequencies := range subjectTab {
+		var plotValues plotter.Values = frequencies
+		bar, err := plotter.NewBarChart(plotValues, w)
+		if err != nil {
+			panic(err)
+		}
+		bar.LineStyle.Width = vg.Length(0)
+		bar.Color = plotutil.Color(i)
+		bar.Offset = vg.Points(w.Points() * (float64(i) - 2))
+		p.Add(bar)
+		p.Legend.Add(house, bar)
+		i++
 	}
-	barsA.LineStyle.Width = vg.Length(0)
-	barsA.Color = plotutil.Color(0)
-	barsA.Offset = -w
 
-	barsB, err := plotter.NewBarChart(groupB, w)
-	if err != nil {
-		panic(err)
-	}
-	barsB.LineStyle.Width = vg.Length(0)
-	barsB.Color = plotutil.Color(1)
-
-	barsC, err := plotter.NewBarChart(groupC, w)
-	if err != nil {
-		panic(err)
-	}
-	barsC.LineStyle.Width = vg.Length(0)
-	barsC.Color = plotutil.Color(2)
-	barsC.Offset = w
-
-	p.Add(barsA, barsB, barsC)
-	p.Legend.Add("Group A", barsA)
-	p.Legend.Add("Group B", barsB)
-	p.Legend.Add("Group C", barsC)
 	p.Legend.Top = true
-	p.NominalX("One", "Two", "Three", "Four", "Five")
 
-	if err := p.Save(5*vg.Inch, 3*vg.Inch, "barchart.png"); err != nil {
+	folder := "./visual/histograms/"
+	os.MkdirAll(folder, 0777)
+	if err := p.Save(vg.Length(nbPortions)*2*vg.Centimeter+vg.Length(2), 10*vg.Centimeter, folder+subject+".png"); err != nil {
 		panic(err)
 	}
+
 }
